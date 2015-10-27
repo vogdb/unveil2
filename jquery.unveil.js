d4a4b96324854e1e3495f110b3301a9860a96e00
Joram van den Boezem <joram@nabble.nl>
2015-10-27 16:44:22 +0100

    Added some features:  - Breakpoints (fixes #1)  - Support for original image in src (fixes #2)

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -8,49 +8,91 @@
  * https://github.com/luis-almeida
  */
 
-;(function($) {
+;
+(function ($) {
 
-  $.fn.unveil = function(threshold, callback) {
+    $.fn.unveil = function (threshold, callback, sizes) {
 
-    var $w = $(window),
-        th = threshold || 0,
-        retina = window.devicePixelRatio > 1,
-        attrib = retina? "data-src-retina" : "data-src",
-        images = this,
-        loaded;
+        // Initialize variables
+        var $window = $(window),
+            pixel = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+            th = threshold || 0,
+            sizes = sizes || [{}],
+            retina = window.devicePixelRatio > 1,
+            images = this,
+            loaded;
 
-    this.one("unveil", function() {
-      var source = this.getAttribute(attrib);
-      source = source || this.getAttribute("data-src");
-      if (source) {
-        this.setAttribute("src", source);
-        if (typeof callback === "function") callback.call(this);
-      }
-    });
+        // Read and reset the src attribute first, to prevent loading
+        // of original images
+        this.each(function () {
+            var $this = $(this);
+            if (!$this.data('src')) {
+                $this.data('src', $this.prop('src'));
+            }
+            var placeholder = $this.data('src-placeholder') || pixel;
+            $this.prop('src', placeholder);
+        });
 
-    function unveil() {
-      var inview = images.filter(function() {
-        var $e = $(this);
-        if ($e.is(":hidden")) return;
+        // This triggers an image source to be set on the target,
+        // based on window width and presence of retina screen
+        this.one("unveil", function () {
+            var $this = $(this), windowWidth = $window.width(),
+                attrib = 'src', src, retinaSrc;
 
-        var wt = $w.scrollTop(),
-            wb = wt + $w.height(),
-            et = $e.offset().top,
-            eb = et + $e.height();
+            // Determine attribute to extract source from
+            for (var i in sizes) {
+                var dataAttrib = sizes[i].attribute.replace(/^data-/, '');
+                if (windowWidth >= sizes[i].minWidth && $this.data(dataAttrib)) {
+                    attrib = dataAttrib;
+                    break;
+                }
+            }
 
-        return eb >= wt - th && et <= wb + th;
-      });
+            // Extract source
+            src = retinaSrc = $this.data(attrib);
 
-      loaded = inview.trigger("unveil");
-      images = images.not(loaded);
-    }
+            // Do we have a retina source?
+            if (src.indexOf("|") !== -1) {
+                retinaSrc = src.split("|")[1];
+                src = src.split("|")[0];
+            }
 
-    $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+            // Change attribute on image
+            if (src) {
+                $this.prop("src", (retina && retinaSrc) ? retinaSrc : src);
+                $this.on('load', unveil);
 
-    unveil();
+                // Fire up the callback if it's a function
+                if (typeof callback === "function") {
+                    callback.call(this);
+                }
+            }
+        });
 
-    return this;
+        function unveil() {
+            var inview = images.filter(function () {
+                var $e = $(this);
+                if ($e.is(":hidden")) return;
 
-  };
+                var wt = $window.scrollTop(),
+                    wb = wt + $window.height(),
+                    et = $e.offset().top,
+                    eb = et + $e.height();
+
+                return eb >= wt - th && et <= wb + th;
+            });
+
+            console.log('now in view', inview.length, 'images');
+            loaded = inview.trigger("unveil");
+            images = images.not(loaded);
+        }
+
+        $window.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+        unveil();
+
+        return this;
+
+    };
 
 })(window.jQuery || window.Zepto);
